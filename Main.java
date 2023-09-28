79dd13b474aa7070acb0275ab4fbd0c38fb19091
indrajeet125 <indrajeetyadav932001@gmail.com>
2023-09-28 15:02:04 +0530

    demo

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(" this is java program");
         System.out.println(" todday date is :" + new Date());
+        System.out.println("this is new line for checking ");
+
     }
+
 }

b26e27910e83ee3bd0229460a886df8c536124d1
indrajeet125 <indrajeetyadav932001@gmail.com>
2023-09-28 14:46:10 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(" this is java program");
+        System.out.println(" todday date is :" + new Date());
+    }
+}
